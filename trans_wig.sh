bde8d2925dd8f71cfdf8c673536c18f9f04e1f02
Luz <779410241@qq.com>
2022-11-25 17:41:35 +0800

    Create trans_wig.sh

---
+++ trans_wig.sh
@@ -0,0 +1,15 @@
+p=0
+cat GSM2236619_Bcatenin.mouse.wig | while read line;do
+	if [ ${line:0:1} = "f" ];then
+		l=`grep "$line" fixedStep.txt | wc -l`
+		if [ $l = 0 ];then
+			p=0
+			echo $line >> fixedStep.txt
+			echo $line >> res.wig
+		else
+			p=1
+		fi
+	elif [ $p = 0 ];then
+		echo $line >> res.wig
+	fi
+done
